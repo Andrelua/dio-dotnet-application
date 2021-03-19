df2d9f3a4cd13fad366d1a6095572c0c467c0869
Andre Luan <andreluan176@gmail.com>
2021-03-19 14:47:47 -0300

    .

--- Dio.bank/Program.cs
+++ Dio.bank/Program.cs
@@ -6,8 +6,56 @@ namespace Dio.bank
     {
         static void Main(string[] args)
         {
-            Conta conta1 = new Conta("André Luan", 0, 0, TipoConta.PessoaFisica);
-            Console.WriteLine(conta1.ToString());
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //ListarContas();
+                        break;
+                    case "2":
+                        //InserirConta();
+                        break;
+                    case "3":
+                        //Transferir();
+                        break;
+                    case "4":
+                        //Sacar();
+                        break;
+                    case "5":
+                        //Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Escolha uma opção válida!")
+                        break;
+                }
+            }
         }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bem-vindos(as) ao DIO Bank!!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1 - Listar contas");
+            Console.WriteLine("2 - Inserir nova conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar a tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine("-> ");
+            return opcaoUsuario;
+        }
+
+        
     }
 }
