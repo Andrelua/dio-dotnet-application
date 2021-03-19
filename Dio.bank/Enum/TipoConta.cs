7647fb23991cb0a375e81c9e139e35fc1dcaf5fe
Andre Luan <andreluan176@gmail.com>
2021-03-19 14:31:40 -0300

    Primeira parte

---
+++ Dio.bank/Enum/TipoConta.cs
@@ -0,0 +1,10 @@
+namespace Dio.bank
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+
+        PessoaJuridica = 2
+
+    }
+}
---
+++ Dio.bank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dio.bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta1 = new Conta("André Luan", 0, 0, TipoConta.PessoaFisica);
+            Console.WriteLine(conta1.ToString());
+        }
+    }
+}
